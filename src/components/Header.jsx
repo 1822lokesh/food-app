35eaadad506b46a9a4be4334fc3ad64c899cc35d
1822lokesh <lokeshharikant4@gmail.com>
2025-06-26 00:03:03 +0530

    some modification between components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,6 @@ const Header = () => {
     <AppBar position="static" sx={{ backgroundColor: '#fff', boxShadow: 'none' }}>
       <Container maxWidth="lg" disableGutters>
         <Toolbar sx={{ display: 'flex', justifyContent: 'space-between', minHeight: 64, px: 0 }}>
-          {/* Logo */}
           <Box sx={{ display: 'flex', alignItems: 'center' }}>
             <Box
               component="img"
@@ -16,8 +15,6 @@ const Header = () => {
               sx={{ width: 40, height: 40, mr: 1 }}
             />
           </Box>
-
-          {/* Navigation Links */}
           <Box sx={{ display: { xs: 'none', md: 'flex' }, gap: 4 }}>
             {['Home', 'Menu', 'Services', 'Food Carts'].map((item) => (
               <Typography key={item} variant="body1" sx={{ color: '#000', cursor: 'pointer' }}>
@@ -25,8 +22,6 @@ const Header = () => {
               </Typography>
             ))}
           </Box>
-
-          {/* Icons */}
           <Box sx={{ display: 'flex', gap: 2 }}>
             <IconButton>
               <SearchIcon sx={{ color: '#000' }} />
